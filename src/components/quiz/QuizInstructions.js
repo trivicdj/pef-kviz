91f130424328fabf1dbff3237aab05d13dcc3b21
Djordje Trivic <trivic95@gmail.com>
2020-12-11 22:55:14 +0100

    Serbian translations

--- src/components/quiz/QuizInstructions.js
+++ src/components/quiz/QuizInstructions.js
@@ -7,52 +7,73 @@ import fiftyFifty from '../../assets/img/fiftyFifty.PNG';
 import hints from '../../assets/img/hints.PNG';
 import options from '../../assets/img/options.PNG';
 
-const QuizInstructions = () => (
-    <Fragment>
-        <Helmet><title>Quiz Instructions - Quiz App</title></Helmet>
-        <div className="instructions container">
-            <h1>How to Play the Game</h1>
-            <p>Ensure you read this guide from start to finish.</p>
-            <ul className="browser-default" id="main-list">
-                <li>The game has a duration of 15 minutes and ends as soon as your time elapses.</li>
-                <li>Each game consists of 15 questions.</li>
-                <li>
-                    Every question contains 4 options.
-                    <img src={options} alt="Quiz App options example" />
-                </li>
-                <li>
-                    Select the option which best answers the question by clicking (or selecting) it.
-                    <img src={answer} alt="Quiz App answer example" />
-                </li>
-                <li>
-                    Each game has 2 lifelines namely:
-                    <ul id="sublist">
-                        <li>2 50-50 chances</li>
-                        <li>5 Hints</li>
-                    </ul>
-                </li>
-                <li>
-                    Selecting a 50-50 lifeline by clicking the icon
-                    <span className="mdi mdi-set-center mdi-24px lifeline-icon"></span>
-                    will remove 2 wrong answers, leaving the correct answer and one wrong answer
-                    <img src={fiftyFifty} alt="Quiz App Fifty-Fifty example"/>
-                </li>
-                <li>
-                    Using a hint by clicking the icon
-                    <span className="mdi mdi-lightbulb-on mdi-24px lifeline-icon"></span>
-                    will remove one wrong answer leaving two wrong answers and one correct answer. You can use as many hints as possible on a single question.
-                    <img src={hints} alt="Quiz App hints example" />
-                </li>
-                <li>Feel free to quit (or retire from) the game at any time. In that case your score will be revealed afterwards.</li>
-                <li>The timer starts as soon as the game loads.</li>
-                <li>Let's do this if you think you've got what it takes?</li>
-            </ul>
-            <div>
-                <span className="left"><Link to="/">No take me back</Link></span>
-                <span className="right"><Link to="/play/quiz">Okay, Let's do this!</Link></span>
-            </div>
-        </div>
-    </Fragment>
+const QuizInstructions = () => ( <
+    Fragment >
+    <
+    Helmet > < title > Quiz Instructions - Quiz App < /title></Helmet >
+    <
+    div className = "instructions container" >
+    <
+    h1 > Uputstvo za korišćenje aplikacije < /h1> <
+    p > Pažljivo pročitaj pravila igre od početka do kraja. < /p> <
+    ul className = "browser-default"
+    id = "main-list" >
+    <
+    li > Igra traje 15 minuta i završava se čim vaše vreme istekne. < /li> <
+    li > Svaka igra sadrži 15 pitanja. < /li> <
+    li >
+    Svako pitanje ima četri ponuđena odgovora. <
+    img src = {
+        options
+    }
+    alt = "Quiz App options example" / >
+    <
+    /li> <
+    li >
+    Klikni na jedno od ponuđenih odgovora. <
+    img src = { answer }
+    alt = "Quiz App answer example" / >
+    <
+    /li> <
+    li >
+    tokom trajanja testa imate mogućnost korišcenja dve vrste pomoći:
+    <
+    ul id = "sublist" >
+    <
+    li > 2 puta pomoc 50 - 50 < /li> <
+    li > 5 puta Hint ili nagoveštaj < /li> < /
+    ul > <
+    /li> <
+    li >
+    Odabirom 50 - 50 pomoći klikom na ikonicu <
+    span className = "mdi mdi-set-center mdi-24px lifeline-icon" > < /span>
+    uklonićete dva netačna odgovora, pri tome ostaje jedan tačan i jedan netačan odgovor <
+    img src = {
+        fiftyFifty
+    }
+    alt = "Quiz App Fifty-Fifty example" / >
+    <
+    /li> <
+    li >
+    Odabirom hinta ili nagoveštaja klikom na ikonicu <
+    span className = "mdi mdi-lightbulb-on mdi-24px lifeline-icon" > < /span>
+    uklonićete jedan netačan odgovor.Ova pomoć se može koristiti više puta u toku jednog pitanja. <
+    img src = { hints }
+    alt = "Quiz App hints example" / >
+    <
+    /li> <
+    li > Kviz možete napustiti u svakom trenutku < /li> <
+    li > Tajmer se pokreće čim se igra učita. < /li> <
+    li > Da li ste spremni da započnete s kvizom ? < /li> < /
+    ul > <
+    div >
+    <
+    span className = "left" > < Link to = "/" > Ne, povratak nazad! < /Link></span >
+    <
+    span className = "right" > < Link to = "/play/quiz" > U redu, započnimo s kvizom! < /Link></span > < /
+    div > <
+    /div> < /
+    Fragment >
 );
 
 export default QuizInstructions;
